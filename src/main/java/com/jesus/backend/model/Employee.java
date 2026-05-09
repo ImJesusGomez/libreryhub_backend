c8505e1ff6256a5416eb0baa645a49c527418a9f
ImJesusGomez <jesusgomez.dev10@gmail.com>
2026-05-09 10:39:02 -0600

    Integración de auditoría en las entidades

--- src/main/java/com/jesus/backend/model/Employee.java
+++ src/main/java/com/jesus/backend/model/Employee.java
@@ -2,11 +2,14 @@ package com.jesus.backend.model;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+
 
 @Entity
 @Table(name = "employees")
-public class Employee {
+public class Employee extends Auditable {
     // Attributes
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -28,4 +31,82 @@ public class Employee {
     private String password;
 
     // Relationships
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "employees_roles",
+            joinColumns = @JoinColumn(name = "employee_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    // Constructor
+    public Employee() {
+    }
+
+    public Employee(UUID id, String firstName, String lastName, String email, String phoneNumber, String password, Set<Role> roles) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    // Getters & Setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
--- src/main/java/com/jesus/backend/model/Role.java
+++ src/main/java/com/jesus/backend/model/Role.java
@@ -2,11 +2,13 @@ package com.jesus.backend.model;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public class Role extends Auditable{
 
     // Attributes
     @Id
@@ -14,4 +16,43 @@ public class Role {
     private UUID id;
 
     private String name;
+
+    // Relationships
+    @ManyToMany(mappedBy = "roles")
+    private Set<Employee> employees = new HashSet<>();
+
+    // Constructor
+    public Role() {
+    }
+
+    public Role(UUID id, String name, Set<Employee> employees) {
+        this.id = id;
+        this.name = name;
+        this.employees = employees;
+    }
+
+    // Getters & Setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
 }
