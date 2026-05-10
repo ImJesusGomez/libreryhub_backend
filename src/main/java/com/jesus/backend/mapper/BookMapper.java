3820a3835b7ab5b94b9f0fb827a6c8a573c6d451
ImJesusGomez <jesusgomez.dev10@gmail.com>
2026-05-10 14:33:19 -0600

    Mejora en la limpieza al momento de inyectar datos en la base de datos

--- src/main/java/com/jesus/backend/mapper/BookMapper.java
+++ src/main/java/com/jesus/backend/mapper/BookMapper.java
@@ -4,13 +4,26 @@ import com.jesus.backend.dto.request.BookCreateRequestDTO;
 import com.jesus.backend.dto.response.BookResponseDTO;
 import com.jesus.backend.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
 
-    // DTO -> Entity
-    Book toEntity(BookCreateRequestDTO dto);
-
     // Entity -> DTO
     BookResponseDTO toBookResponseDTO(Book book);
+
+    // DTO -> Entity
+    @Mapping(target = "isbn", source = "isbn", qualifiedByName = "trim")
+    @Mapping(target = "title", source = "title", qualifiedByName = "trim")
+    @Mapping(target = "author", source = "author", qualifiedByName = "trim")
+    @Mapping(target = "editorial", source = "editorial", qualifiedByName = "trim")
+    @Mapping(target = "synopsis", source = "synopsis", qualifiedByName = "trim")
+    Book toEntity(BookCreateRequestDTO dto);
+
+    // Funciones de limpieza
+    @Named("trim")
+    default String trim(String attribute) {
+        return attribute == null ? null : attribute.trim();
+    }
 }
